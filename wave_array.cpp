587ed27f13121442b22e03f5f2edd57493687570
Hiral Jain <71397276+HiralJain112@users.noreply.github.com>
2021-06-16 23:28:45 +0530

    Create wave_array.cpp

---
+++ wave_array.cpp
@@ -0,0 +1,15 @@
+/*
+Given an array of integers, sort the array into a wave like array and return it,
+In other words, arrange the elements into a sequence such that a1 >= a2 <= a3 >= a4 <= a5.....
+*/
+
+vector<int> Solution::wave(vector<int> &A) 
+{
+    sort(A.begin(),A.end());
+
+    for(int i = 1;i < A.size();i+=2)
+    {
+       swap(A[i-1],A[i]);
+    }
+    return A;
+}
