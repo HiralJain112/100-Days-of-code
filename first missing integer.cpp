c68a9dd5c742fde197f7524910dff47f5395a27f
Hiral Jain <71397276+HiralJain112@users.noreply.github.com>
2021-06-18 23:22:13 +0530

    Create first missing integer.cpp

---
+++ first missing integer.cpp
@@ -0,0 +1,19 @@
+int firstMissingPositive(int* A, int n1) {
+    int n = A.size();
+            for (int i = 0; i < n; i++) 
+            {
+                if (A[i] > 0 && A[i] <= n) 
+                {
+                    int pos = A[i] - 1;
+                    if (A[pos] != A[i])
+                     {
+                        swap(A[pos], A[i]);
+                        i--;
+                    }
+                }
+            }
+            for (int i = 0; i < n; i++) {
+                if (A[i] != i + 1) return (i + 1);
+            }
+            return n + 1;        
+}
