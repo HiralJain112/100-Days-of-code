3330963dbb87538f589613b977212f3f1e242af3
Hiral Jain <71397276+HiralJain112@users.noreply.github.com>
2021-07-13 23:34:35 +0530

    Create smallest divisile .cpp

---
+++ smallest divisile .cpp
@@ -0,0 +1,9 @@
+long long getSmallestDivNum(long long n){
+        // code here
+        long long ans = 1;
+        for(long long i = 1; i <= n; i++)
+        {
+            ans = (ans * i)/(__gcd(ans,i)); // lcm*hcf = a*b
+        }
+        return ans;
+    }
