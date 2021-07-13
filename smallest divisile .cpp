72740c828d8b8acd60ca511b19a55f36065b1190
Hiral Jain <71397276+HiralJain112@users.noreply.github.com>
2021-07-13 23:35:24 +0530

    Update smallest divisile .cpp

--- smallest divisile .cpp
+++ smallest divisile .cpp
@@ -1,6 +1,6 @@
 long long getSmallestDivNum(long long n){
         // code here
-        long long ans = 1;
+        long long ans = 1 ;
         for(long long i = 1; i <= n; i++)
         {
             ans = (ans * i)/(__gcd(ans,i)); // lcm*hcf = a*b
